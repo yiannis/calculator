f327f0bc5e7e653d7eff5bb73f45794d205aae25
Yiannis Belias <yiannisbe@gmail.com>
2012-04-22 21:36:40 +0300

    Fix indentation

--- Token.h
+++ Token.h
@@ -41,58 +41,58 @@ enum TokenType {
 
 
 struct Token {
-    Token(TokenType type, int pos) :
-      m_type(type), m_pos(pos)
-    {}
-    Token( Function::ID id, int pos ) :
-      m_type(FUNCTION), m_pos(pos)
-    {
-      m_data.id = id;
-    }
-    Token( float value, int pos ) :
-      m_type(FLOAT), m_pos(pos)
-    {
-      m_data.value = value;
-    }
-    Token( float *pvalue, int pos ) :
-      m_type(CONSTANT), m_pos(pos)
-    {
-      m_data.pValue = pvalue;
-    }
+  Token(TokenType type, int pos) :
+    m_type(type), m_pos(pos)
+  {}
+  Token( Function::ID id, int pos ) :
+    m_type(FUNCTION), m_pos(pos)
+  {
+    m_data.id = id;
+  }
+  Token( float value, int pos ) :
+    m_type(FLOAT), m_pos(pos)
+  {
+    m_data.value = value;
+  }
+  Token( float *pvalue, int pos ) :
+    m_type(CONSTANT), m_pos(pos)
+  {
+    m_data.pValue = pvalue;
+  }
 
-    std::string toString() const
-    {
-      std::string str;
-      std::ostringstream out;
+  std::string toString() const
+  {
+    std::string str;
+    std::ostringstream out;
 
-      switch (m_type) {
-        case FUNCTION:
-          str = Function::IDtoName( m_data.id );
-          break;
-        case FLOAT:
-          out << "(" << m_data.value << ")";
-          str = out.str();
-          break;
-        case CONSTANT:
-          out << "(" << *(m_data.pValue) << ")";
-          str = out.str();
-          break;
-        default:
-          str = tokenToString(m_type);
-      }
-
-      return str;
+    switch (m_type) {
+      case FUNCTION:
+        str = Function::IDtoName( m_data.id );
+        break;
+      case FLOAT:
+        out << "(" << m_data.value << ")";
+        str = out.str();
+        break;
+      case CONSTANT:
+        out << "(" << *(m_data.pValue) << ")";
+        str = out.str();
+        break;
+      default:
+        str = tokenToString(m_type);
     }
 
-    static const std::string tokenToString(TokenType t);
+    return str;
+  }
 
-    TokenType m_type;
-    int m_pos;
-    union {
-      Function::ID id;
-      float value;
-      float* pValue;
-    } m_data;
+  static const std::string tokenToString(TokenType t);
+
+  TokenType m_type;
+  int m_pos;
+  union {
+    Function::ID id;
+    float value;
+    float* pValue;
+  } m_data;
 };
 
 #endif //TOKEN_H
