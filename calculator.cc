742a3d4a718d81e8d1a9ed039f27ebc7f19f209a
Yiannis Belias <yiannisbe@gmail.com>
2012-05-13 10:03:28 +0300

    Use Compiler in calculator

--- calculator.cc
+++ calculator.cc
@@ -4,6 +4,7 @@
 #include <map>
 
 #include "Interpreter.h"
+#include "Compiler.h"
 
 using namespace std;
 
@@ -11,35 +12,44 @@ typedef map<string,float>::const_iterator iter;
 
 int main(int argc, char* argv[])
 {
-  int error = 0;
+  int error = 0, dump = 0;
   string source;
 
   if (argc == 1) {
-    cerr << "Usage: " << argv[0] << "<math-expression> [<var1> <value1>] [<var2> <value2>] ..." << endl;
+    cerr << "Usage: " << argv[0] << " [-d] <math-expression> [<var1> <value1>] [<var2> <value2>] ..." << endl;
     return 1;
-  } else {
-    source = argv[1];
+  } else if (argv[1][0] == '-' && argv[1][1] == 'd') {
+    dump = 1;
   }
+  source = argv[1+dump];
 
-  istringstream input(source);
+  istringstream input(source), inputc(source);
   Interpreter engine(&input);
+  Compiler llvmc(&inputc);
 
-  int args_left = argc-2;
+  int args_left = argc-2-dump;
   int pairs = args_left/2;
   int end = 2 + 2*pairs;
-  for (int arg=2; arg<end; arg+=2)
+  for (int arg=2+dump; arg<end; arg+=2) {
     engine.set( argv[arg], atof(argv[arg+1]) );
+    llvmc.set( argv[arg], atof(argv[arg+1]) );
+  }
+  llvmc.emmitIR();
 
   for (iter i=engine.getConstantsTable().begin(); i!=engine.getConstantsTable().end(); i++)
     cout << i->first << " = " << i->second << ", ";
-  cout << "\n" << source;
+  cout << "\n" << source << ":" << endl;
 
   try {
-      cout << " = " << engine.result() << endl;
+      cout << "(native)= " << engine.result() << endl;
+      cout << "(llvm)= " << llvmc.result() << endl;
   } catch (exception& e) {
     cerr << e.what() << endl;
     error = 1;
   }
+
+  if (dump)
+    llvmc.printIR();
 
   return error;
 }
