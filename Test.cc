57abc01ae56cadd56b5ce215b26454e378742378
Yiannis Belias <yiannisbe@gmail.com>
2012-05-15 18:13:43 +0300

    Fix nasty little bug & add a few more tests.
    The llvm visitor class did not check if the constant was a built-in.
    
    The AST classes were not built with llvm in mind
    and that's beginning to show...

--- Test.cc
+++ Test.cc
@@ -1,4 +1,5 @@
 #include <sstream>
+#include <cmath>
 
 #include <QString>
 
@@ -9,6 +10,17 @@
 #include "Test.h"
 
 using namespace std;
+
+/// "0.4*cos((2*pi/150)*t)*sin(x+y)/(x^2+y^2)"
+double Test::func1d( double x, double y, double t )
+{
+  return 0.4*cos((2*M_PI/150)*t)*sin(x+y)/(pow(x,2)+pow(y,2));
+}
+/// "0.4*cos((2*pi/150)*t)*sin(x+y)/(x^2+y^2)"
+float Test::func1f( float x, float y, float t )
+{
+  return 0.4*cos((2*M_PI/150)*t)*sin(x+y)/(pow(x,2)+pow(y,2));
+}
 
 void Test::lexerTest()
 {
@@ -29,6 +41,27 @@ void Test::lexerTest()
   QCOMPARE( lexer.nextToken().m_type, MULT );
   QCOMPARE( lexer.nextToken().m_type, CONSTANT );
   QCOMPARE( lexer.nextToken().m_type, END );
+
+
+  istringstream source_pi("pi");
+  Lexer lexer_pi(&source_pi);
+
+  Token pi = lexer_pi.nextToken();
+  QCOMPARE( pi.m_type, CONSTANT );
+  QCOMPARE( pi.m_name, string("pi") );
+  QCOMPARE( *pi.m_data.pValue, Lexer::S_PI );
+}
+
+void Test::constantsTest()
+{
+  istringstream i_input("pi+e"), c_input("pi+e");
+
+  Interpreter engine(&i_input);
+  Compiler llvmc(&c_input);
+  llvmc.compile();
+
+  QCOMPARE( engine.result(), Lexer::S_PI+Lexer::S_E );
+  QCOMPARE( llvmc.result(), Lexer::S_PI+Lexer::S_E );
 }
 
 void Test::parserTest_data()
@@ -89,6 +122,49 @@ void Test::updateValuesTest()
   QCOMPARE( llvmc.result(), 81.0F );
 }
 
+void Test::updateCompilerValuesTest()
+{
+  string function("x+y+z");
+  istringstream input(function);
+
+  Compiler llvmc(&input);
+  llvmc.set( "x", 0 );
+  llvmc.set( "y", 0 );
+  llvmc.set( "z", 0 );
+  llvmc.compile();
+
+  for (float x=0; x<100; x++) {
+    llvmc.set( "x", x );
+
+    QCOMPARE( llvmc.result(), x );
+  }
+  llvmc.set( "x", 0 );
+
+  for (float y=0; y<100; y++) {
+    llvmc.set( "y", y );
+
+    QCOMPARE( llvmc.result(), y );
+  }
+  llvmc.set( "y", 0 );
+
+  for (float z=0; z<100; z++) {
+    llvmc.set( "z", z );
+
+    QCOMPARE( llvmc.result(), z );
+  }
+  llvmc.set( "z", 0 );
+
+  for (float x=0; x<100; x++)
+    for (float y=0; y<100; y++)
+      for (float z=0; z<100; z++) {
+        llvmc.set( "x", x );
+        llvmc.set( "y", y );
+        llvmc.set( "z", z );
+
+        QCOMPARE( llvmc.result(), x+y+z );
+      }
+}
+
 void Test::CompilerTest_data()
 {
   parserTest_data();
@@ -110,14 +186,14 @@ void Test::CompilerTest()
   QCOMPARE( llvmMath.result(), result );
 }
 
-void Test::CompilerVsInterpreterTest_data()
+void Test::CompilerVsInterpreterBenchmark_data()
 {
   QTest::addColumn<bool>("useCompiler");
   QTest::newRow("Compiler") << true;
   QTest::newRow("Interpreter") << false;
 }
 
-void Test::CompilerVsInterpreterTest()
+void Test::CompilerVsInterpreterBenchmark()
 {
   QFETCH(bool, useCompiler);
 
@@ -144,5 +220,53 @@ void Test::CompilerVsInterpreterTest()
 
   QCOMPARE( result, 100.0F );
 }
+
+void Test::fullValuesCompilerTest()
+{
+  istringstream input(func1_str);
+
+  Compiler llvmc(&input);
+  llvmc.set( "x", 0.0F );
+  llvmc.set( "y", 0.0F );
+  llvmc.set( "t", 0.0F );
+  llvmc.compile();
+
+  for (float x=-5.0F; x<5.0F; x+=0.2F)
+    for (float y=-5.0F; y<5.0F; y+=0.2F)
+      for (float t=0.0F; t<100.0F; t++) {
+        llvmc.set( "x", x );
+        llvmc.set( "y", y );
+        llvmc.set( "t", t );
+
+//        QCOMPARE( func1d( x, y, t ), (double)llvmc.result() );
+        double native_result = func1d( x, y, t ),
+               llvm_result = llvmc.result(),
+               diff = abs(native_result - llvm_result);
+        QVERIFY( abs(diff/native_result) < 0.001 );
+      }
+  cerr << "\n";
+}
+
+void Test::fullValuesInterpreterTest()
+{
+  istringstream input(func1_str);
+
+  Interpreter engine(&input);
+  engine.set( "x", 0.0F );
+  engine.set( "y", 0.0F );
+  engine.set( "t", 0.0F );
+
+  for (float x=-5.0F; x<5.0F; x+=0.2F)
+    for (float y=-5.0F; y<5.0F; y+=0.2F)
+      for (float t=0.0F; t<100.0F; t++) {
+        engine.set( "x", x );
+        engine.set( "y", y );
+        engine.set( "t", t );
+
+        QCOMPARE( func1f( x, y, t ), engine.result() );
+      }
+}
+
+std::string Test::func1_str = "0.4*cos((2*pi/150)*t)*sin(x+y)/(x^2+y^2)";
 
 QTEST_MAIN(Test)
--- Test.h
+++ Test.h
@@ -9,20 +9,30 @@ class Test: public QObject
   Q_OBJECT
 
     private:
+      static std::string func1_str;
+
+      double func1d( double x, double y, double t );
+      float func1f( float x, float y, float t );
 
     private slots:
       void lexerTest();
+
+      void constantsTest();
 
       void parserTest();
       void parserTest_data();
 
       void updateValuesTest();
+      void updateCompilerValuesTest();
 
       void CompilerTest();
       void CompilerTest_data();
 
-      void CompilerVsInterpreterTest();
-      void CompilerVsInterpreterTest_data();
+      void CompilerVsInterpreterBenchmark();
+      void CompilerVsInterpreterBenchmark_data();
+
+      void fullValuesCompilerTest();
+      void fullValuesInterpreterTest();
 };
 
 #endif //TEST_H
